7808c676f9835a425458b34ad966a35e3d3e85d5
Abhinav Mani <abhinav1946@gmail.com>
2020-06-27 12:11:47 +0530

    SignInForm state managment added

--- src/Components/Form/SignInForm.js
+++ src/Components/Form/SignInForm.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'semantic-ui-react'
 const SignInForm = () => {
+    const [usename,setUsername] = useState("");
+    const [password,setPassword] = useState("");
+    const [possition,setPostion] = useState('');
     return ( 
         <Form>
             <Form.Field>
-            <label>Username</label>
-            <input placeholder='Username' />
+                <label>Username</label>
+                <input placeholder='Username' value={usename} onChange={e=>setUsername(e.target.value)} />
             </Form.Field>
             <Form.Field>
-            <label>Password</label>
-            <input placeholder='Password' type={'password'}/>
+                <label>Password</label>
+                <input placeholder='Password' type={'password'} value={password} onChange={e=>setPassword(e.target.value)}/>
             </Form.Field>
-            <Form.Field label='Post' control='select'>
-                <option value='none'>Select One</option>
+            <Form.Field label='Post' control='select' value={possition} onChange={e=>setPostion(e.target.value)}>
+                <option value=''>Select One</option>
                 <option value='doctor'>Doctor</option>
                 <option value='receptionist'>Receptionist</option>
                 <option value='pharmsist'>Pharmsist</option>
