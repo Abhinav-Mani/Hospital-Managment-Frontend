94cf7f640aff4283c0791480037e499b8e5e0d6b
Abhinav Mani <abhinav1946@gmail.com>
2020-07-04 19:52:16 +0530

    Email for future appointment added

--- src/Components/Doctor/Appointments/PatientProfile.js
+++ src/Components/Doctor/Appointments/PatientProfile.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Card } from 'semantic-ui-react'
+import { Card ,Icon,Button} from 'semantic-ui-react'
 
 const PatientProfile = ({data}) => {
     return ( 
+        <Card.Group>
         <Card fluid>
             <Card.Content>
                 <Card.Header>{data[6]} {data[7]}</Card.Header>
@@ -13,6 +14,18 @@ const PatientProfile = ({data}) => {
             <Card.Description>{data[0]}</Card.Description>
             </Card.Content>
         </Card>
+        <Card fluid color='red'>
+            <Card.Content>
+                <Card.Header>
+                    Email Patient For Future Appointment
+                    <Button color='red' floated={'right'}>
+                        <Icon name='mail' />
+                        Email
+                    </Button>
+                </Card.Header>
+            </Card.Content>
+        </Card>
+         </Card.Group>
      );
 }
  
