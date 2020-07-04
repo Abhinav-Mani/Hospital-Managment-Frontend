dec6a693fb66ce64b8b70645ffe01d61398cb186
Abhinav Mani <abhinav1946@gmail.com>
2020-07-04 21:15:34 +0530

    Precscrption List added

--- src/Components/Doctor/Appointments/PatientProfile.js
+++ src/Components/Doctor/Appointments/PatientProfile.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import { Card ,Icon,Button} from 'semantic-ui-react'
+import React, { useState } from 'react';
+import { Card ,Icon,Button,List,Form} from 'semantic-ui-react'
 import api from '../../../api';
 
 const PatientProfile = ({data}) => {
+    const [sending,setSending] = useState(false); 
+    const [medicinces,setMedinces] =useState(["Med1","Med2"]);
     const mail=()=>{
+        setSending(true);
         const querryParams={
             email:data[7],
             message:"You are requested for further appointment with doctor"
         }
-        api.mail(querryParams);
+        api.mail(querryParams)
+        .then(res=>console.log(res))
+        .catch(err=>console.log(err))
+        .finally(()=>setSending(false));
     }
     return ( 
         <Card.Group>
@@ -26,11 +32,25 @@ const PatientProfile = ({data}) => {
             <Card.Content>
                 <Card.Header>
                     Email Patient For Future Appointment
-                    <Button color='red' floated={'right'} onClick={()=>mail()}>
+                    <Button color='red' floated={'right'} loading={sending} onClick={()=>mail()}>
                         <Icon name='mail' />
                         Email
                     </Button>
                 </Card.Header>
+            </Card.Content>
+        </Card>
+        <Card fluid>
+            <Card.Content >
+                <List divided relaxed>
+                    {medicinces.map(medeicine=>(
+                        <List.Item>
+                            {medeicine}
+                        </List.Item>
+                    ))}
+                </List>
+                <Form>
+                    
+                </Form>
             </Card.Content>
         </Card>
          </Card.Group>
